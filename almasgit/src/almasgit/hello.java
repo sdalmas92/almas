bb081bd5c4e4cf74796886b66281b77b16ea95f8
sdalmas92 <99801120+sdalmas92@users.noreply.github.com>
2022-02-21 19:06:52 +0530

    Update hello.java

--- almasgit/src/almasgit/hello.java
+++ almasgit/src/almasgit/hello.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class hello {
 public static void main(String[] args) {
 	System.out.println("hello world");
-	System.out.println("hellow");
 	String str1 = "almas";
 	String[] str = { "vivo", "sam" };
 	System.out.println(str1);
