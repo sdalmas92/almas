86bc831c1b49d9b6215128f77026f5d01f4337de
sdalmas92 <99801120+sdalmas92@users.noreply.github.com>
2022-02-21 18:45:57 +0530

    Update hello.java

--- almasgit/src/almasgit/hello.java
+++ almasgit/src/almasgit/hello.java
@@ -3,5 +3,7 @@ package almasgit;
 public class hello {
 public static void main(String[] args) {
 	System.out.println("hello world");
+	//please push & commit outside git
+	//please pull & commit inside code
 }
 }
